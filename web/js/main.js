7ba892020b216a3ad8535470bd215066c243d684
Charlie Backus <cbackus123@gmail.com>
2015-10-07 08:40:52 -0400

    dynamic, basic css implemented. dev environment set up

---
+++ web/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+  // compile our template
+ /* var template = Handlebars.compile($("#people-template").html());
+
+  // add the person partial
+  Handlebars.registerPartial("person", $("#project-partial").html());
+
+  var data = {
+    info: [
+        {
+            wemade: {
+                title: "For Students, By Students",
+                body: "We are a team of Developers, Designers, and Business Analysts. Our team is composed entirely of Rochester Institute of Technology students, working to improve online academic services for current and future students. "
+            },
+            wedo: {
+                title: "Real Work. Real Experience.",
+                body: "Members of our team are dedicated to top-notch communication, leadership, technology, and critical thinking. In our day-to-day work we interact with customers, give presentations, and document our processes allowing many opportunities to develop effective communication skills. We research possible opportunities to improve and innovate future solutions, and we work to ensure a great experience for the end user. The SITO team is guided by senior management, who act as mentors and provide support."
+            },
+            weare: {
+                title: "Create. Develop. Execute",
+                body: "We see our projects through from conception to production. Starting with students and campus personnel, we seek to understand user/client needs and find the best solution. Throughout design and development, we create interfaces with the end user in mind. We perform usability and functionality tests to verify requirements and user needs are met before release. We operate as a business, establish best practices, and determine the best approach for growing the organization."
+            }
+        }
+    ]
+};
+
+$('#list').html(template(data));
+*/
+});
